379a0d8021f92e404235af0cd102c934d7c881ca
Ana Ferreira <Ana.Ferreira@ibm.com>
2024-02-28 11:07:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ app.get("/", function(req,res){
     res.send("Seja bem vindo, node js!")
 })
 
-app.get("/cadastrar/:item/:quantidade", function(req,res){
-    res.send("Página de cadastro" + req.params.item + "-Quantidade " + req.params.quantidade)
+app.get("/produtos/:automovel/:marca/:modelo/:ano", function(req,res){
+    res.send("Página de Produtos " + req.params.automovel + "-Marca " + req.params.marca + "-Modelo " + req.params.modelo + "-Marca " + req.params.ano)
+})
+
+app.get("/cadastrar/:usuario", function(req,res){
+    res.send("Página de cadastro " + req.params.usuario)
 })
 
 app.get("/contato", function(req,res){
-    res.send("página de contato")
+    res.send("Página de contato")
 })
+
 
